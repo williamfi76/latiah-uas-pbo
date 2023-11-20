5777105787f7046402edb76c17bd6287ef8b9340
williamfi76 <wfrederickiv76@gmail.com>
2023-11-20 21:24:19 +0700

    Program Insert name

---
+++ insertName.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+
+    std::string name;
+
+    std::cout << "Insert your Name: ";
+    std::cin >> name;
+
+    std::cout << "==============================================\n";
+    std::cout << "Hello " << name << "! I am a C++ program.";
+
+    return 0;
+}
